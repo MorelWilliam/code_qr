519f7c4d87eb9fb02baf737657261ab7144661e3
William Morel <morelwilliam2602@gmail.com>
2024-04-08 23:01:30 -0400

    reseau

--- website_code_qr/script.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementsByClassName("shareButton").addEventListener("click", function() {
-    // URL de partage d'Instagram avec le message personnalisé
-    var shareUrl = "https://www.instagram.com/share?url=" + encodeURIComponent(window.location.href) + "&amp;title=" + encodeURIComponent("Message personnalisé ici");
-
-    // Ouvrir la fenêtre de partage
-    window.open(shareUrl, "_blank");
-});
